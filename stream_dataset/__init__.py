97381dfb88ea55d89c6a3689583f476801bd45b7
Naowak <yannis.bendiouis@gmail.com>
2026-04-04 19:14:54 +0200

    label acc for multi_label

--- stream_dataset/__init__.py
+++ stream_dataset/__init__.py
@@ -24,7 +24,7 @@ def compute_score(Y, Y_hat, prediction_timesteps, category, threshold=0.5):
     - Y (np.ndarray): Target array [B, T, O]
     - Y_hat (np.ndarray): Predicted array [B, T, O]
     - prediction_timesteps (list): List of prediction timesteps
-    - category (str): Category of the task -> 'classification' (acc) or 'regression' (mse) or 'multi_classification' (exact match acc)
+    - category (str): Category of the task -> 'classification' (acc) or 'regression' (mse) or 'multi_classification'
 
     Returns:
     - accuracy (float): Accuracy value
@@ -54,7 +54,7 @@ def compute_score(Y, Y_hat, prediction_timesteps, category, threshold=0.5):
         preds = np.stack(preds, axis=0)  # [B, prediction_timesteps] int: class
         truths = np.stack(truths, axis=0)  # [B, prediction_timesteps] int: class
         preds_bin = (sigmoid(preds) >= threshold).astype(int)
-        correct_samples = np.all(preds_bin == truths, axis=(1, 2))
+        correct_samples = np.all(preds_bin == truths)
         score = 1 - np.mean(correct_samples)
 
     elif category=='regression':
